cf1f89197b98ec4c9ee38176644cf90c2dd47a72
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-08-24 19:26:47 +1000

    spring boot auto conf

--- src/main/java/com/in28minutes/springboot/basic/springin10steps/SpringIn10StepsApplication.java
+++ src/main/java/com/in28minutes/springboot/basic/springin10steps/SpringIn10StepsApplication.java
@@ -2,12 +2,17 @@ package com.in28minutes.springboot.basic.springin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn10StepsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringIn10StepsApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = SpringApplication.run(SpringIn10StepsApplication.class, args);
+		for (String bean:applicationContext.getBeanDefinitionNames()) {
+			System.out.println(" Beans : "+bean);
+		}
+    }
 
 }
